1d3ca1c26361918f3880134a08a1763a5306330e
dominhit215@gmail.com <dominhit215@gmail.com>
2024-06-29 12:56:56 +0700

    fix code

--- api/v1/routes/admin/account.route.js
+++ api/v1/routes/admin/account.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../../controllers/admin/account.controller");
-
+//Staff
 router.get("/staff" , controller.getStaff);
 
 router.post("/staff/create" , controller.createStaff);
